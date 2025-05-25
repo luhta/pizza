999a686f274d934f58680c51c23d3c91c0e71c6c
luhta <luhta.pro@yandex.ru>
2025-05-25 10:19:26 +0300

    fix:favicon.ico

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="m-10 text-2xl text-amber-800">Начало пути</h1>
+      <h1 className="    m-10 text-2xl text-amber-800">Начало пути</h1>
     </div>
   );
 }
